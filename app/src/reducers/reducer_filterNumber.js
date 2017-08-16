0e37f22a3e9009fc029624e5847b220989db0a26
YInglai Xu <yinglai@kth.se>
2017-08-16 11:05:44 +0200

    bug fix

--- app/src/reducers/reducer_filterNumber.js
+++ app/src/reducers/reducer_filterNumber.js
@@ -1,9 +1,11 @@
-import { SET_FILTER_NUMBER } from '../actions/index';
+import { SET_FILTER_NUMBER, SEARCH_TOPICS } from '../actions/index';
 
 export default function(state = 0, action){
     switch(action.type){
         case SET_FILTER_NUMBER:
             return action.payload;
+        case SEARCH_TOPICS:
+            return action.payload.length;
     }
     return state;
 }
